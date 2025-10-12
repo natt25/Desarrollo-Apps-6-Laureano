7a8507b86a9773f4d5232c4508ac035dcdcfd253
natty <nattyjimena@gmail.com>
2025-10-12 15:02:29 -0500

    updates in project

--- FASE-2/Lab6/Ejercicios/mi-proyecto/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./App.css";             // Estilos globales
-import App from "./App";        // Componente raíz
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
